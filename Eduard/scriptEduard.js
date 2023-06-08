98501caacaee02d6cf6eaeb97450758745a909ca
edward375 <rotarueduard91@gmail.com>
2023-06-08 14:24:24 -0700

    add event edward

---
+++ Eduard/scriptEduard.js
@@ -0,0 +1,6 @@
+const anghelina = document.querySelector(".squareAnghelina");
+anghelina.addEventListener("onmouseenter", (ev)=>{
+    const edy = document.createElement("div");
+    edy.classList.add("squareEduard")
+    edy.textContent = "Eduard";
+})
