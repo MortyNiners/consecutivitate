365997eb7bb8622d0722fa0a04a4ed39cb76de9a
dorin <cujba.dorin29@gmail.com>
2023-06-08 15:19:14 +0300

    commit3

--- Dorin/scriptDorin.js
+++ Dorin/scriptDorin.js
@@ -5,20 +5,14 @@ dima.style.width = '300px';
 dima.style.height = '300px';
 dima.style.backgroundColor = 'red';
 
-dima.addEventListener("keypress", (e) => {
-    if (e.shiftKey) {
-        dima.addEventListener("keypress", (e) => {
-            if (e.code === 9) {
-                dima.addEventListener("keypress", (e) => {
-                    if (e.code === 13) {
-                        dorin.style.width = '300px';
-                        dorin.style.height = '300px';
-                        dorin.style.backgroundColor = 'blue';
+dima.addEventListener("click", (e) => {
+    dorin.style.width = '300px';
+    dorin.style.height = '300px';
+    dorin.style.backgroundColor = 'blue';
 
-                    }
-                })
-            }
-        })
+}
+)
 
-    }
-})
+
+
+
